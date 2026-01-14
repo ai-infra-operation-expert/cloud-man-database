ea8aefbe8f01f68a0c6a7b117dd0eb521e943797
Allen Galler <allengaller@qq.com>
2026-01-14 12:29:52 +0800

    bugfix
    bugfix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  root: resolve(__dirname),
+  server: {
+    fs: {
+      strict: false,
+      allow: [resolve(__dirname)]
+    }
+  },
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true
+  }
 })
